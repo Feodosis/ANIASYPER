5b44a0614cc545f0383550aa64262aafac861cc2
Feodosis <144526217+Feodosis@users.noreply.github.com>
2023-12-09 13:38:08 +0200

    Add files via upload

---
+++ AAAA.py
@@ -0,0 +1,88 @@
+
+#a = float(input("Введіть довжину в метрах"))
+#km = a / 1000
+#mm = a * 1000
+#print("довжина в кілометрах:", km)
+#print("довжина в міліметрах:", mm)
+
+#num1 = float(input("Введіть перше число:"))
+# = float(input("Введіть друге число:"))
+#num3 = float(input("Введіть третє число:"))
+#num4 = float(input("Введіть четверте число:"))
+#num5 = float(input("Введіть п'яте число:"))
+#a = (num1 + num2 + num3 + num4 + num5) / 5
+#print(a)
+
+#num1 = int(input("Введіть перше число"))
+#num2 = int(input("Введіть друге число"))
+#num3 = int(input("Введіть третє число"))
+#if num1 > num2 + num3:
+    #a = num1 + num2 + num3
+    #print("Сума всіх чисел:", a)
+#else:
+    #b = num2 - num3
+    #print("Різниця між дргим і третім числом:", b)
+
+#num1 = int(input("Введіть перше число:"))
+#num2 = int(input("Введіть друге число:"))
+#num3 = int(input("Введіть третє число:"))
+#if num1 + num2 == num3:
+    #print("Третє число найбільше")
+#else:
+    #result = num1 * num2
+    #print("Результат множення першого і другого числа:", result)
+
+#a = int(input("Введіть номер місяця:"))
+#if a == 1:
+    #print("Січень")
+#elif a == 2:
+    #print("Лютий")
+#elif a == 3:
+    #print("Березень")
+#elif a == 4:
+    #print("Квітень")
+#elif a == 5:
+    #print("Травень")
+#elif a == 6:
+    #print("Червень")
+#elif a == 7:
+    #print("Липень")
+#elif a == 8:
+    #print("Серпень")
+#elif a == 9:
+    #print("Вересень")
+#elif a == 10:
+    #print("Жовтень")
+#elif a == 11:
+    #print("Листопад")
+#elif a == 12:
+    #print("Грудень")
+#else:
+    #print("Невірний номер місяця")
+
+#age = int(input("Введіть ваш вік:"))
+#if age <= 11:
+    #category = "дитина"
+#elif age <= 17:
+    #category = "підліток"
+#elif age <= 65:
+    #category = "дорослий"
+#else:
+    #category = "пенсіонер"
+#print("Ви є", category)
+
+#username = "username"
+#valid_login = False
+#attempts = 0
+#while not valid_login:
+    #login = input("Введіть логін:")
+    #if login == username:
+        #valid_login = True
+        #print("Ви ввели правильний логін")
+    #else:
+        #attempts += 1
+        #print("Неправильний логін")
+    #print("Кількість спроб:", attempts)
+
+
+
